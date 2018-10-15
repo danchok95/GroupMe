9162cef6072a1e0730210ec37d5e0c23bdec3b82
Wilson Chang <ckm_5801@yahoo.com>
2018-10-15 21:24:17 +0800

    Resync project

---
+++ app/src/main/java/com/mobileminiproject/wilsondaniel/groupme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mobileminiproject.wilsondaniel.groupme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
